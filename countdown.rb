545de351d67d61a6c98e3af4aff6271cb22d0c1e
Joel Hobgood <jhobgoo@gmail.com>
2020-02-16 22:59:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,10 +7,10 @@ def countdown(x)
   while x > 0
     puts "#{x} SECOND(S)!"
   x -= 1  
-  sleep(1)
   end
-def countdown_with_sleep(n)
-  sleep(n)
+  
+def countdown_with_sleep(x)
+  sleep(1)
 end  
   'HAPPY NEW YEAR!'
 end
